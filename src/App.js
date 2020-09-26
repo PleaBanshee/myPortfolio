5d592976ef8f50812607eff7dda73fbbd687201d
jumphoto <jumanahphotography@gmail.com>
2020-09-26 17:24:57 +0200

    initail commit completed mynavbar component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//components for navbar
+
+import MyNavbar from "./components/my-navbar/mynavbar.component";
+
+import './App.css';
+
+
+const App = () => {
+  return (
+    <div>
+      <MyNavbar/>
+      My Portfolio
+    </div>
+  );
+}
+
+export default App;
